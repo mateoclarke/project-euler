b6ae02351fd963710c4782aa39d5c1a77931e4a6
mateoclarke <code@mateoclarke.com>
2013-11-13 11:22:50 -0600

    final answer with sum

--- Multi3and5.rb
+++ Multi3and5.rb
@@ -4,10 +4,13 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-for i in (1..1000)
+arr = []
+for i in (1..999)
 if i % 3 == 0
-	puts i
+	arr.push(i)
 elsif i % 5 == 0
-	puts i
+	arr.push(i)
 end
 end
+
+arr.inject(:+)
